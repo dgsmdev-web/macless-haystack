81d3d03a908d7f7aaa233acfeb27b88b2a3fb360
dgsmdev-web <dgsmdev@gmail.com>
2026-07-27 18:41:22 +0300

    Update MainActivity.kt

--- macless_haystack/android/app/src/main/kotlin/de/dchristl/headlesshaystack/MainActivity.kt
+++ macless_haystack/android/app/src/main/kotlin/de/dchristl/headlesshaystack/MainActivity.kt
@@ -1,1 +1,19 @@
+package de.dchristl.headlesshaystack
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+
+      override fun onCreate(savedInstanceState: Bundle?) {
+    WindowCompat.setDecorFitsSystemWindows(getWindow(), false)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+      splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
+    }
+
+    super.onCreate(savedInstanceState)
+  }
+}
